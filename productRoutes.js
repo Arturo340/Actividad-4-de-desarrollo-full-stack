f81a522f71ec4d96abc69efc3ada49af7e771628
Arturo340 <al02989129@tecmilenio.mx>
2026-02-25 10:16:57 -0600

    Delete productRoutes.js

--- productRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const productController = require('../controllers/productController');
-const authMiddleware = require('../middlewares/authMiddleware');
-
-router.post('/', authMiddleware, productController.createProduct);
-router.get('/', authMiddleware, productController.getProducts);
-
-module.exports = router;
